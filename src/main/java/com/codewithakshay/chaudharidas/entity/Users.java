1a46e3e084bb3be5bc8aed127f548b7c806693bd
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-06-15 22:48:18 +0530

    user entity created

---
+++ src/main/java/com/codewithakshay/chaudharidas/entity/Users.java
@@ -0,0 +1,32 @@
+package com.codewithakshay.chaudharidas.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(schema = "chaudharidas", name = "users")
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private long userId;
+	@NotBlank(message = "User Name is Mandatory")
+	@Column(name = "user_name")
+	private String userName;
+	@NotBlank(message = "Email is Mandatory")
+	private String email;
+	@NotBlank(message = "Password is Mandatory")
+	private String password;
+	@NotBlank(message = "Role is Mandatory")
+	private String role;
+
+}

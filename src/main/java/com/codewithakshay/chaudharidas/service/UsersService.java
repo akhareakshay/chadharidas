8fd8503d17484bb5935bb44ae059be3c01d00d7a
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-06-15 23:13:25 +0530

    users service searching method created

---
+++ src/main/java/com/codewithakshay/chaudharidas/service/UsersService.java
@@ -0,0 +1,11 @@
+package com.codewithakshay.chaudharidas.service;
+
+import java.util.List;
+
+import com.codewithakshay.chaudharidas.entity.Users;
+
+public interface UsersService {
+
+	public List<Users> searchUser(Users users);
+
+}

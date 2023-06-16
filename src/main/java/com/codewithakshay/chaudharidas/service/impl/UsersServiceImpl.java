c5fb73912d29cf55701acb52bd47b77c48e2ad93
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-06-16 19:49:12 +0530

    created implimented class for users

---
+++ src/main/java/com/codewithakshay/chaudharidas/service/impl/UsersServiceImpl.java
@@ -0,0 +1,19 @@
+package com.codewithakshay.chaudharidas.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.codewithakshay.chaudharidas.entity.Users;
+import com.codewithakshay.chaudharidas.service.UsersService;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+
+	@Override
+	public List<Users> searchUser(Users users) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

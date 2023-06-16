e6f56493f413ec5db950f4d92b8626c10bfbf4ef
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-06-16 19:48:20 +0530

    created post category entity for category of the post

---
+++ src/main/java/com/codewithakshay/chaudharidas/entity/PostCategory.java
@@ -0,0 +1,25 @@
+package com.codewithakshay.chaudharidas.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+@Entity
+@Table(schema = "chaudharidas", name = "post_category")
+@Data
+public class PostCategory {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "post_category_id")
+	private long postCategoryId;
+	@NotBlank(message = "Name is Mandatory")
+	private String name;
+
+}

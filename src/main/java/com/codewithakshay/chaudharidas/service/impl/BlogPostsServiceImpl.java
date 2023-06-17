51315abc9cddbd65b28e5d716294c4d6c061c499
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-06-17 11:47:02 +0530

    service implimentation for posts

---
+++ src/main/java/com/codewithakshay/chaudharidas/service/impl/BlogPostsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.codewithakshay.chaudharidas.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.codewithakshay.chaudharidas.entity.BlogPosts;
+import com.codewithakshay.chaudharidas.service.BlogPostsService;
+
+@Service
+public class BlogPostsServiceImpl implements BlogPostsService {
+
+	@Override
+	public List<BlogPosts> searchBlogPosts(BlogPosts blogPosts) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

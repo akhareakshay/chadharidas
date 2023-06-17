cd69c5d10a7f67c70180a4f54aaffb8e9c7dde88
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-06-17 11:41:49 +0530

    author created

--- src/main/java/com/codewithakshay/chaudharidas/entity/Author.java
+++ src/main/java/com/codewithakshay/chaudharidas/entity/Author.java
@@ -1,5 +1,26 @@
 package com.codewithakshay.chaudharidas.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(schema = "chaudharidas", name = "author")
 public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "authori_id")
+	private long authorId;
+	@NotBlank(message = "Author Name is Mandatory")
+	private String name;
+	@NotBlank(message = "Author Email is Mandatory")
+	private String email;
+	@NotBlank(message = "Author Bio is Mandatory")
+	private String bio;
 
 }

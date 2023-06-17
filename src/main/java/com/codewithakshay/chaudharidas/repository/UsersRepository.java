6c1f992939c32dfc843d4aa5fdd4449050e923b4
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-06-17 11:44:18 +0530

    changes

--- src/main/java/com/codewithakshay/chaudharidas/repository/UsersRepository.java
+++ src/main/java/com/codewithakshay/chaudharidas/repository/UsersRepository.java
@@ -1,9 +1,11 @@
 package com.codewithakshay.chaudharidas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.codewithakshay.chaudharidas.entity.Users;
 
+@Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
 }
